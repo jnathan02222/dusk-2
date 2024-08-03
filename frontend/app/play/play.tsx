e92b0875072f8348b6a2da59d1d2d129af5531f7
Nathan <test@gmail.com>
2024-08-03 00:56:08 -0400

    Better canvas focusing

--- frontend/app/play/play.tsx
+++ frontend/app/play/play.tsx
@@ -6,9 +6,19 @@ import Script from 'next/script'
 export default function Play({signedIn} : {signedIn : boolean}) {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
-  
+  const focusCanvas = (event : React.MouseEvent<HTMLElement>) => {
+    const elem : HTMLElement = event.target as HTMLElement;
+    if(elem.nodeName != "MAIN" && elem.nodeName != "DIV"){
+      return;
+    }
+    console.log("FOCUS")
+    if(canvasRef.current){
+      canvasRef.current.focus();
+    } 
+  }
+
   return (
-    <main className="flex min-h-screen justify-center	items-center p-24">
+    <main onClick={focusCanvas} className="flex min-h-screen justify-center	items-center p-24">
       {signedIn && <script src="https://sdk.scdn.co/spotify-player.js"></script>}
       {signedIn && <script src="spotifyPlayer.js"></script>}
       <Script type="text/javascript" charSet="utf-8" src={signedIn ? "scenario30445.js" : "scenario30446.js"} ></Script>
@@ -18,12 +28,14 @@ export default function Play({signedIn} : {signedIn : boolean}) {
       <div className='relative text-white' style={{width: 960}}>
         <div className={signedIn ? "" : "hidden"}><SearchBar></SearchBar></div>
         <canvas ref={canvasRef} className="outline-none rounded-lg mb-4 mt-4" tabIndex={0} id="scenarioCanvas" width="972" height="662">Your browser does not support the canvas tag.</canvas>
+        <div className="absolute w-full text-center text-gray-500	-z-20">Hint: Press space to start running in-game!</div>
         <button  id="playButton" className="rounded-md border-2 p-2 hover:border-fuchsia-200 text-center w-20">Run</button>
+       
         <button className="hidden" disabled={true} id="resetButton" >Reset</button>
         <input className="hidden" type="range" id="speedSlider" min={0} max={99} disabled={true}></input>
             
       </div>
-      <div className="text-white absolute w-full flex min-h-screen justify-center	items-center -z-10">
+      <div  className="text-white absolute w-full flex min-h-screen justify-center	items-center -z-10">
         <p>JavaScript may have failed to load. Wait a minute before reloading the page.</p>
       </div>
     </main>
--- frontend/server.js
+++ frontend/server.js
@@ -64,12 +64,11 @@ var spotifyApiRequest = function(url, req, res){
     }
   });
 }
+async function setupBrowser(){
+  browser = await puppeteer.launch({headless:true});
+}
 
 async function addSpotifyUser(email){
-  //Open browser
-  if(browser === null){
-    browser = await puppeteer.launch({headless:true});
-  }
   const page = await browser.newPage();
   await page.setViewport({width: 1920, height: 1024});
 
@@ -103,183 +102,187 @@ var app = next({dev : dev});
 const handle = app.getRequestHandler();
 //Start Next app
 app.prepare().then( () => {
-  var server = express();
-  server.use(cors());
-  server.use(cookieParser());
-  server.use(express.json());
-
-  //Endpoints
-
-  //Redirect to Spotify authenticator with parameters
-  server.get('/login', function(req, res) {
-    var state = generateRandomString(16);
+  setupBrowser().then(
+    () => {
+      console.log(browser)
+      var server = express();
+      server.use(cors());
+      server.use(cookieParser());
+      server.use(express.json());
     
-    res.cookie(stateKey, state);
-    var scope = 'user-read-private user-read-email streaming user-modify-playback-state';
+      //Endpoints
     
-    res.redirect('https://accounts.spotify.com/authorize?' +
-      querystring.stringify({
-        response_type: 'code',
-        client_id: client_id,
-        scope: scope,
-        redirect_uri: redirect_uri,
-        state: state
-      }));
-  });
-
-  //For requesting an access token
-  server.get('/callback', function(req, res) {
-    //Get token based on code
-    var code = req.query.code || null;
-    var state = req.query.state || null;
-
-    //Check state
-    var storedState = req.cookies ? req.cookies[stateKey] : null;
-  
-    if (state === null || state !== storedState) {
-      console.log("State mismatch.")
-      res.redirect('/');
-    } else {
-      res.clearCookie(stateKey);
-      var authOptions = {
-        url: 'https://accounts.spotify.com/api/token',
-        form: {
-          code: code,
-          redirect_uri: redirect_uri,
-          grant_type: 'authorization_code'
-        },
-        headers: {
-          'content-type': 'application/x-www-form-urlencoded',
-          Authorization: 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
-        },
-        json: true
-      };
-  
-      request.post(authOptions, function(error, response, body) {
-        if (!error && response.statusCode === 200) {
-          res.cookie('spotify_token', body.access_token);
-
-          res.redirect('/play');
+      //Redirect to Spotify authenticator with parameters
+      server.get('/login', function(req, res) {
+        var state = generateRandomString(16);
+        
+        res.cookie(stateKey, state);
+        var scope = 'user-read-private user-read-email streaming user-modify-playback-state';
+        
+        res.redirect('https://accounts.spotify.com/authorize?' +
+          querystring.stringify({
+            response_type: 'code',
+            client_id: client_id,
+            scope: scope,
+            redirect_uri: redirect_uri,
+            state: state
+          }));
+      });
+    
+      //For requesting an access token
+      server.get('/callback', function(req, res) {
+        //Get token based on code
+        var code = req.query.code || null;
+        var state = req.query.state || null;
+    
+        //Check state
+        var storedState = req.cookies ? req.cookies[stateKey] : null;
+      
+        if (state === null || state !== storedState) {
+          console.log("State mismatch.")
+          res.redirect('/');
         } else {
-          console.log(response.statusCode)
-          res.redirect('/unexpected-error');
+          res.clearCookie(stateKey);
+          var authOptions = {
+            url: 'https://accounts.spotify.com/api/token',
+            form: {
+              code: code,
+              redirect_uri: redirect_uri,
+              grant_type: 'authorization_code'
+            },
+            headers: {
+              'content-type': 'application/x-www-form-urlencoded',
+              Authorization: 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
+            },
+            json: true
+          };
+      
+          request.post(authOptions, function(error, response, body) {
+            if (!error && response.statusCode === 200) {
+              res.cookie('spotify_token', body.access_token);
+    
+              res.redirect('/play');
+            } else {
+              console.log(response.statusCode)
+              res.redirect('/unexpected-error');
+            }
+          });
         }
       });
-    }
-  });
-
-  server.get('/play', (req, res) => {
-    // use the access token to access the Spotify Web API
     
-    checkToken(req, (response) => {
-      //Note, methods like redirect end the http response of the res object and should not be called more than once on the same object
-      //Handle
-      if(response.statusCode === 200){
-        return handle(req, res);
-      //User not added
-      }else if(response.statusCode === 403){
-        res.redirect('/unregistered-user');
-      //Invalid token
-      }else if(response.statusCode === 401){
-        console.log("Invalid token.")
-        res.redirect('/');
-      }else{
-        res.redirect('/unexpected-error');
-      }
-    });
-    
-  });
-
-
-  server.get("/unregistered-user", (req, res)=>{
-    //Attempt request
-    checkToken(req, (response) => {
-      if(response.statusCode === 403){
-        return handle(req, res);
-      }else{
-        res.redirect('/play?' +
-          querystring.stringify({
-            signed_in: true
-          }));
-      }
-    });
-  });
-
-  server.put("/add-user", (req, res) => {
-    var options = {
-      url: 'https://api.spotify.com/v1/me',
-      headers: { 'Authorization': 'Bearer ' + req.cookies['spotify_token'] },
-      json: true
-    };
-    request.get(options, function(error, response, body) {
-      if(!error && response.statusCode === 200){
-        res.status(200).send("OK");
-      }else if(!error && response.statusCode === 403){
-        addSpotifyUser(req.body.email).then(
-          ()=>{
-            request.get(options, function(error, response, body) {
-              if(!error && response.statusCode === 200){
-                res.status(200).send("OK");
-              }else{
-                res.status(500).send("Something went wrong.");
-              }
-            });
+      server.get('/play', (req, res) => {
+        // use the access token to access the Spotify Web API
+        
+        checkToken(req, (response) => {
+          //Note, methods like redirect end the http response of the res object and should not be called more than once on the same object
+          //Handle
+          if(response.statusCode === 200){
+            return handle(req, res);
+          //User not added
+          }else if(response.statusCode === 403){
+            res.redirect('/unregistered-user');
+          //Invalid token
+          }else if(response.statusCode === 401){
+            console.log("Invalid token.")
+            res.redirect('/');
+          }else{
+            res.redirect('/unexpected-error');
           }
-        );
-      }else{
-        res.status(500).send("Invalid email.");
-      }
-    });
-  });
-
-  server.get("/spotify-search", (req, res) => {
-    spotifyApiRequest('https://api.spotify.com/v1/search?'+querystring.stringify({q : req.query.q, type : "track", limit:10}), req, res);
-  });
-
-  server.get("/spotify-audio-analysis", (req, res) => {
-    spotifyApiRequest('https://api.spotify.com/v1/audio-analysis/'+req.cookies['track_id'], req, res);
-  });
-
-  server.get("/spotify-token", (req, res) => {
-    res.json({token : req.cookies['spotify_token']});
-  });
-
-  server.put("/start-webplayer", (req, res) => {
-    url = `https://api.spotify.com/v1/me/player/play?device_id=${req.query.device_id}`;
-    body = {uris: ["spotify:track:"+req.cookies['track_id']]};
+        });
+        
+      });
     
-    var options = {
-      url: url,
-      body : body,
-      headers: { 'Authorization': 'Bearer ' + req.cookies['spotify_token'] },
-      json: true
-    };
-    request.put(options, function(error, response, body) {
-      if(error){
-        console.error('Error starting playback: ', error);
-        res.status(500).send({ error: 'Failed to fetch data from Spotify API' })
-      }else{
-        res.status(response.statusCode).send(body);
-      }
-    });
     
-  });
-
-  server.put("/track-id", (req, res) => {
-    res.cookie('track_id', req.body.track_id);
-    res.status(201).json({ message : 'success'});
-  });
-
-
-  server.all("*", (req, res) => {
-    return handle(req, res);
-  });
-
-  //Start server
-  console.log(`Listening on ${port}`);
-  server.listen(port);
-});
-
+      server.get("/unregistered-user", (req, res)=>{
+        //Attempt request
+        checkToken(req, (response) => {
+          if(response.statusCode === 403){
+            return handle(req, res);
+          }else{
+            res.redirect('/play?' +
+              querystring.stringify({
+                signed_in: true
+              }));
+          }
+        });
+      });
+    
+      server.put("/add-user", (req, res) => {
+        var options = {
+          url: 'https://api.spotify.com/v1/me',
+          headers: { 'Authorization': 'Bearer ' + req.cookies['spotify_token'] },
+          json: true
+        };
+        request.get(options, function(error, response, body) {
+          if(!error && response.statusCode === 200){
+            res.status(200).send("OK");
+          }else if(!error && response.statusCode === 403){
+            addSpotifyUser(req.body.email).then(
+              ()=>{
+                request.get(options, function(error, response, body) {
+                  if(!error && response.statusCode === 200){
+                    res.status(200).send("OK");
+                  }else{
+                    res.status(500).send("Something went wrong.");
+                  }
+                });
+              }
+            );
+          }else{
+            res.status(500).send("Invalid email.");
+          }
+        });
+      });
+    
+      server.get("/spotify-search", (req, res) => {
+        spotifyApiRequest('https://api.spotify.com/v1/search?'+querystring.stringify({q : req.query.q, type : "track", limit:10}), req, res);
+      });
+    
+      server.get("/spotify-audio-analysis", (req, res) => {
+        spotifyApiRequest('https://api.spotify.com/v1/audio-analysis/'+req.cookies['track_id'], req, res);
+      });
+    
+      server.get("/spotify-token", (req, res) => {
+        res.json({token : req.cookies['spotify_token']});
+      });
+    
+      server.put("/start-webplayer", (req, res) => {
+        url = `https://api.spotify.com/v1/me/player/play?device_id=${req.query.device_id}`;
+        body = {uris: ["spotify:track:"+req.cookies['track_id']]};
+        
+        var options = {
+          url: url,
+          body : body,
+          headers: { 'Authorization': 'Bearer ' + req.cookies['spotify_token'] },
+          json: true
+        };
+        request.put(options, function(error, response, body) {
+          if(error){
+            console.error('Error starting playback: ', error);
+            res.status(500).send({ error: 'Failed to fetch data from Spotify API' })
+          }else{
+            res.status(response.statusCode).send(body);
+          }
+        });
+        
+      });
+    
+      server.put("/track-id", (req, res) => {
+        res.cookie('track_id', req.body.track_id);
+        res.status(201).json({ message : 'success'});
+      });
+    
+    
+      server.all("*", (req, res) => {
+        return handle(req, res);
+      });
+    
+      //Start server
+      console.log(`Listening on ${port}`);
+      server.listen(port);
+    });      
+  }
+);
 
 /*
 var options = {
